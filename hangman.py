490cae6e2b30255683c666ef7cc1e6858dcba752
Freddy B <52885763+ingxfm@users.noreply.github.com>
2022-01-12 19:05:30 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -3,7 +3,7 @@ import random
 word_list = ["aardvark", "baboon", "camel", "farmer", "Obama", "Jordan"]
 
 chosen_word = random.choice(word_list)
-print(chosen_word)
+#print(chosen_word)
 
 guess = input("Make a guess. Enter a letter: ").lower()
 
